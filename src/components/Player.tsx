4543c46ba0f0f1bd6fc905019ed077ebfcc36042
james <james.barbosa@gmail.com>
2024-02-04 09:13:23 +0800

    Updated player styling

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,11 +1,22 @@
+import { useState } from "react";
 import "./Player.css";
 
-export function Player({symbol, isActive}) {
+export function Player({ symbol, isActive }) {
+    const [isEditMode, setisEditMode] = useState(false)
+    const [playerName, setPlayerName] = useState("<Enter name>")
+
     return <>
-        <div className={`player flex ${isActive ? "active" : ''}`}>
-            <label>Name: </label>
-            <input type="text"></input>
+        <div className={`player-container flex`}>
             <span className="symbol">{symbol}</span>
+
+            <section className={`player flex ${isActive ? "active" : ''}`}>
+                <label>Name: </label>
+                {
+                    isEditMode ? <input className="name" onChange={(e) => { setPlayerName(e.target.value) }} type="text"></input> : <label className="name">{playerName}</label>
+                }
+
+                <span className="edit" onClick={() => { setisEditMode((prev) => !prev) }}>{isEditMode ? "Submit" : "Edit"}</span>
+            </section>
         </div>
     </>
 }
